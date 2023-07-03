9df09d293f26a1b4d46a9d702de568bca97c862e
Benhur <ben@alteos.com>
2023-07-03 22:46:36 +0200

    html-to-virtualdom

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-const world = 'world';
+import * as inlineCss from 'inline-css';
+import {readFileSync} from 'fs';
+import * as htmlparser2 from 'htmlparser2';
+import {getHtmlParserHandler} from './html-parser-handler';
+import {printVirtualDom} from './helper/printVirtualDom';
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
-}
+(async () => {
+  const html = readFileSync(
+    '/Users/benhur/Documents/personal/git/nodejs-html-to-pdf/mocks/test1.html',
+    {encoding: 'utf-8'}
+  );
+  const inlineCssHtml = await inlineCss(html, {url: 'somefile'});
 
-console.log(hello());
+  const htmlParserHandler = getHtmlParserHandler();
+
+  const parser = new htmlparser2.Parser(htmlParserHandler.htmlParserHandlers);
+
+  parser.write(inlineCssHtml);
+  parser.end;
+
+  const virtualDOM = htmlParserHandler.getVirtualDOM();
+  printVirtualDom(virtualDOM);
+})();
